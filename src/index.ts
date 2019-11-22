64dce27a8d5731889faaa69e10709d1c9b99bca4
Nikolas Burk <nikolas.burk@gmail.com>
2019-11-22 10:20:18 +0100

    minor improvements

--- src/index.ts
+++ src/index.ts
@@ -8,7 +8,6 @@ import {
   stringArg,
 } from 'nexus'
 import { Photon } from '@generated/photon'
-import { argsToArgsConfig } from 'graphql/type/definition'
 
 const photon = new Photon()
 
@@ -20,9 +19,12 @@ const User = objectType({
     t.string('email')
     t.list.field('posts', {
       type: 'Post',
-      resolve: parent => photon.users.findOne({
-        where: { id: parent.id }
-      }).posts()
+      resolve: parent =>
+        photon.users
+          .findOne({
+            where: { id: parent.id },
+          })
+          .posts(),
     })
   },
 })
@@ -36,13 +38,15 @@ const Post = objectType({
     t.boolean('published')
     t.field('author', {
       type: 'User',
-      resolve: parent => photon.posts.findOne({
-        where: { id: parent.id }
-      }).author()
+      resolve: parent =>
+        photon.posts
+          .findOne({
+            where: { id: parent.id },
+          })
+          .author(),
     })
   },
 })
-
 
 const Query = queryType({
   definition(t) {
@@ -56,17 +60,19 @@ const Query = queryType({
       type: 'User',
       nullable: true,
       args: { id: idArg() },
-      resolve: (_, args) => photon.users.findOne({
-        where: { id: args.id }
-      }),
+      resolve: (_, args) =>
+        photon.users.findOne({
+          where: { id: args.id },
+        }),
     })
 
-    // t.list.field('feed', {
-    //   type: 'Post',
-    //   resolve: () => photon.posts.findMany({
-    //     where: { published: true}
-    //   })
-    // }
+    t.list.field('feed', {
+      type: 'Post',
+      resolve: () =>
+        photon.posts.findMany({
+          where: { published: true },
+        }),
+    })
   },
 })
 
@@ -75,15 +81,16 @@ const Mutation = mutationType({
     t.field('createUser', {
       type: 'User',
       args: {
-        name: stringArg({nullable: true}),
+        name: stringArg({ nullable: true }),
         email: stringArg(),
       },
-      resolve: (_, args) => photon.users.create({
-        data: {
-          name: args.name,
-          email: args.email
-        }
-      })
+      resolve: (_, args) =>
+        photon.users.create({
+          data: {
+            name: args.name,
+            email: args.email,
+          },
+        }),
     })
 
     t.field('updateUser', {
@@ -91,15 +98,16 @@ const Mutation = mutationType({
       args: {
         id: idArg(),
         name: stringArg({ nullable: true }),
-        email: stringArg({ nullable: true })
+        email: stringArg({ nullable: true }),
       },
-      resolve: (_, args) => photon.users.update({
-        where: { id: args.id },
-        data: {
-          name: args.name,
-          email: args.email
-        }
-      })
+      resolve: (_, args) =>
+        photon.users.update({
+          where: { id: args.id },
+          data: {
+            name: args.name,
+            email: args.email,
+          },
+        }),
     })
 
     t.field('createDraft', {
@@ -107,30 +115,32 @@ const Mutation = mutationType({
       args: {
         title: stringArg(),
         content: stringArg({ nullable: true }),
-        authorEmail: stringArg()
+        authorEmail: stringArg(),
       },
-      resolve: (_, args) => photon.posts.create({
-        data: {
-          title: args.title,
-          content: args.content,
-          author: {
-            connect: {
-              email: args.authorEmail
-            }
-          }
-        }
-      })
+      resolve: (_, args) =>
+        photon.posts.create({
+          data: {
+            title: args.title,
+            content: args.content,
+            author: {
+              connect: {
+                email: args.authorEmail,
+              },
+            },
+          },
+        }),
     })
 
     t.field('publish', {
       type: 'Post',
       args: {
-        postId: idArg()
+        postId: idArg(),
       },
-      resolve: (_, args) => photon.posts.update({
-        where: { id: args.postId },
-        data: { published: true }
-      })
+      resolve: (_, args) =>
+        photon.posts.update({
+          where: { id: args.postId },
+          data: { published: true },
+        }),
     })
   },
 })
